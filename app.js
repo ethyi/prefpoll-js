ec105608e734ccd1369daddace0cd1b1df4397cc
ethyi <ethyi@proton.me>
2022-07-30 21:06:05 -0700

    MVP

--- app.js
+++ app.js
@@ -10,35 +10,112 @@ const path = require("path");
 let state = {};
 app.use("/static", express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  res.render("creation", {state})
+  res.render("creation", { state });
 });
 
 // parse request body to suitable object
-function parseBody(body) {
+function parseBody(id, body) {
   const question = body.question;
   delete body.question;
-  const options = Object.values(body).filter(element => {
-    return element !== '';
+  const options = Object.values(body).filter((element) => {
+    return element !== "";
   });
-  return { question, options}; 
+  const results = {};
+  return { id, question, options, results };
 }
 
 // create new poll
 app.post("/vote", (req, res) => {
   const id = Math.random().toString(36).slice(2); // repeat after 70M expected
-  const poll = parseBody(req.body);
+  const poll = parseBody(id, req.body);
+  // TODO export to db here
   state[id] = poll;
-  res.redirect("/vote/"+id);
+  res.redirect("/vote/" + id);
 });
 
 app.get("/vote/:paramName", (req, res) => {
   const id = req.params.paramName;
+
+  // TODO import to db here
   const poll = state[id];
-  res.render("vote", {poll})
+  res.render("vote", { poll });
 });
+
+function addVote(id, order) {
+  let poll = state[id];
+  let results = poll.results;
+  let i = order.toString();
+  results[i] = (results[i] || 0) + 1;
+  // TODO add to DB here
+}
+
+// n is numoptions, p is permutations, p = O(n!), altogether O(pn^3)->(n!n^3)
+function calculateOrder(result, numOptions) {
+  let order = []; // could use hashmap for quicker contains, takes up memory?
+  let removed = [];
+  // O(p)
+  let total = Object.values(result).reduce((acc, val) => acc + val, 0);
+  // O(n-1 + n-2+ n-3 ...) = O(n^2) worst case, average O(n)
+  while (order.length < numOptions) {
+    let [minPercentage, minOption] = [1, 0];
+    let [maxPercentage, maxOption] = [0, 0];
+    let tallies = {};
+    // O(p)
+    Object.entries(result).forEach(([key, value]) => {
+      // O(n)
+      key = key.split(",");
+      let index = 0;
+      let option = key[index];
+      // O(n)
+      while (order.includes(option) || removed.includes(option)) {
+        option = key[++index];
+      }
+      let votes = (tallies[option] || 0) + value;
+      tallies[option] = votes;
+
+      let tallyPercentage = votes / total;
+      if (tallyPercentage < minPercentage) {
+        [minPercentage, minOption] = [tallyPercentage, option];
+      }
+      if (tallyPercentage > maxPercentage) {
+        [maxPercentage, maxOption] = [tallyPercentage, option];
+      }
+    });
+
+    if (maxPercentage <= 0.5) {
+      removed.push(minOption);
+    } else {
+      order.push(maxOption);
+      removed = [];
+    }
+  }
+  return order;
+}
+
+app.post("/results", (req, res) => {
+  console.log(req.body);
+  const id = req.body.id;
+  const order = req.body.order;
+  addVote(id, order);
+  // TODO somehow cache it? maybe unnecessary
+  let poll = state[id];
+  let numOptions = poll.options.length;
+  let results = poll.results;
+  poll.rankings = calculateOrder(results, numOptions);
+  res.redirect("/vote/" + id + "/r");
+});
+
+app.get("/vote/:paramName/r", (req, res) => {
+  const id = req.params.paramName;
+  // TODO import from DB here
+  const poll = state[id];
+  console.log(poll.rankings);
+  res.render("results", { poll });
+});
+
 app.listen(port, function () {
   console.log(`server is running on port ${port}.`);
 });
--- public/script.js
+++ public/script.js
@@ -24,4 +24,48 @@ function createInput(event) {
 }
 
 const element = document.getElementById("option3");
-element.addEventListener("input", createInput)
+if (element) {
+element.addEventListener("input", createInput);}
+
+// dragging script
+const draggables = document.querySelectorAll('.draggable')
+const container = document.getElementById("options-container")
+
+draggables.forEach(draggable => {
+  draggable.addEventListener('dragstart', () => {
+    draggable.classList.add('dragging')
+  })
+
+  draggable.addEventListener('dragend', (e) => {
+    e.preventDefault();
+    draggable.classList.remove('dragging')
+
+  })
+  
+})
+
+
+  container.addEventListener('dragover', e => {
+    e.preventDefault()
+    const afterElement = getDragAfterElement(e.clientY)
+    const draggable = document.querySelector('.dragging')
+    if (afterElement == null) {
+      container.appendChild(draggable)
+    } else {
+      container.insertBefore(draggable, afterElement)
+    }
+  })
+
+function getDragAfterElement(y) {
+  const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+  return draggableElements.reduce((closest, child) => {
+    const box = child.getBoundingClientRect()
+    const offset = y - box.top - box.height / 2
+    if (offset < 0 && offset > closest.offset) {
+      return { offset: offset, element: child }
+    } else {
+      return closest
+    }
+  }, { offset: Number.NEGATIVE_INFINITY }).element
+}
